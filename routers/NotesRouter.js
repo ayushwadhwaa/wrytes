eea4cbf5262163c11a92b5543ea8e38054a94e6f
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-08-04 21:01:00 +0530

    Returent content using hbs if note already exists

--- routers/NotesRouter.js
+++ routers/NotesRouter.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const Note = require('./../models/Note');
 const NotesRouter = express.Router();
 NotesRouter.get('', (req, res)=>{
     res.render('index');
 });
-NotesRouter.get('/notes', (req, res)=> {
-    res.render('notes');
+NotesRouter.get('/notes', async (req, res)=> {
+    if(req.query.noteId){
+        const note = await Note.findOne({noteId: req.query.noteId});
+        if(note){
+            res.render('notes',{
+                content: note.content
+            });
+        }else{
+            res.render('notes',{
+                content: "Hello!"
+            });
+        }
+    }else{
+        res.render('notes',{
+            content: "No Pervious Content"
+        });
+    }
+});
+NotesRouter.post('/saveNote', async (req, res)=>{
+    const preNote = await Note.findOne({noteId: req.query.noteId});
+    if(preNote){
+        preNote.content = req.body.content;
+        await preNote.save();
+        res.send("Updated");
+    }else{
+        const note = new Note({
+            noteId: req.query.noteId,
+            content: req.body.content
+        });
+        try{
+            await note.save();
+        }catch(err){
+            console.log(err);
+        }
+        res.send("Saved");
+    }
 });
 module.exports = NotesRouter;
--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 const NotesRouter = require('./../routers/NotesRouter');
 const publicDirectoryPath = path.join(__dirname, '../public');
+require('./../db/mongoose');
 app.use(express.static(publicDirectoryPath));
+app.use(express.json());
 app.set('view engine', 'hbs');
 app.use(NotesRouter);
 app.listen(3000, ()=>{
