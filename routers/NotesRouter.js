22fa14841d3666e7c05f986703be77fb42de3639
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-08-04 23:40:46 +0530

    Added condition to delete a note in NotesRouter

--- routers/NotesRouter.js
+++ routers/NotesRouter.js
@@ -23,22 +23,38 @@ NotesRouter.get('/notes', async (req, res)=> {
     }
 });
 NotesRouter.post('/saveNote', async (req, res)=>{
-    const preNote = await Note.findOne({noteId: req.query.noteId});
-    if(preNote){
-        preNote.content = req.body.content;
-        await preNote.save();
-        res.send({status: "updated"});
-    }else{
-        const note = new Note({
-            noteId: req.query.noteId,
-            content: req.body.content
-        });
-        try{
-            await note.save();
-        }catch(err){
-            console.log(err);
+    if(req.body.content.length === 0){
+        const preNote = await Note.findOne({noteId: req.query.noteId});
+        if(preNote){
+            try{
+                await Note.deleteOne({noteId: req.query.noteId});
+                res.send({status: "Deleted"})
+            }catch(err){
+                res.send("Error");
+            }   
         }
-        res.send({status:"saved"});
+    }else{
+        const preNote = await Note.findOne({noteId: req.query.noteId});
+        if(preNote){
+            preNote.content = req.body.content;
+            try{
+                await preNote.save();
+                res.send({status: "updated"});
+            }catch(err){
+                res.send("Error");
+            }
+        }else{
+            const note = new Note({
+                noteId: req.query.noteId,
+                content: req.body.content
+            });
+            try{
+                await note.save();
+                res.send({status:"saved"});
+            }catch(err){
+                res.send("Error");
+            }
+        }
     }
 });
 module.exports = NotesRouter;
